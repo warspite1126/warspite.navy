296f35c557b6f78569e794f1024db73893f00fee
warspitenavy <dev@warspite.navy>
2020-12-02 03:12:07 +0900

    add

---
+++ assets/include/header.php
@@ -0,0 +1,16 @@
+<header>
+  <a href="/" class="title"><img src="assets/img/warspite.navy.svg" alt="warspite.navy"></a>
+  <nav>
+    <ul>
+      <a href="login" class="noDecoration02 navLi b01">
+        <li>Sign in</li>
+      </a>
+      <a href="account" class="noDecoration02 navLi b01">
+        <li>Account</li>
+      </a>
+      <a href="about" class="noDecoration02 navLi">
+        <li>About</li>
+      </a>
+    </ul>
+  </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/style.css">
+  <link rel="stylesheet" href="style/header.css">
+  <link rel="stylesheet" href="style/index.css">
+  <title>warspite.navy</title>
+</head>
+
+<body>
+  <!-- <script src="assets/include/header.js"></script> -->
+  <?php include(dirname(__FILE__).'./assets/include/header.php'); ?>
+  <div id="mainDiv">
+    <img src="assets/img/BiggerThanAtoms.svg">
+  </div>
+</body>
+
+</html>
