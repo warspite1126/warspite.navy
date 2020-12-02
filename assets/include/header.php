0db40cbf4b040831d104e426580e2f7e0c3e21da
warspitenavy <dev@warspite.navy>
2020-12-03 01:19:09 +0900

    add

--- assets/include/header.php
+++ assets/include/header.php
@@ -2,13 +2,13 @@
   <a href="/" class="title"><img src="assets/img/warspite.navy.svg" alt="warspite.navy"></a>
   <nav>
     <ul>
-      <a href="login" class="noDecoration02 navLi b01">
+      <a href="login" class="noDecoration02 navLi header_li_left">
         <li>Sign in</li>
       </a>
-      <a href="account" class="noDecoration02 navLi b01">
-        <li>Account</li>
+      <a href="account" class="noDecoration02 navLi">
+        <li>Service</li>
       </a>
-      <a href="about" class="noDecoration02 navLi">
+      <a href="about" class="noDecoration02 navLi header_li_right">
         <li>About</li>
       </a>
     </ul>
--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style/style.css">
   <link rel="stylesheet" href="style/header.css">
   <link rel="stylesheet" href="style/index.css">
@@ -11,9 +13,11 @@
 
 <body>
   <!-- <script src="assets/include/header.js"></script> -->
-  <?php include(dirname(__FILE__).'/assets/include/header.php'); ?>
-  <div id="mainDiv">
-    <img src="assets/img/BiggerThanAtoms.svg">
+  <?php include(dirname(__FILE__) . '/assets/include/header.php'); ?>
+  <div id="wrapper">
+    <div id="svgDiv"></div>
+    <!-- <img src="assets/img/BiggerThanAtoms.svg"> -->
+    <!-- <php include(dirname(__FILE__) . '/assets/img/BiggerThanAtoms.svg'); ?> -->
   </div>
 </body>
 
